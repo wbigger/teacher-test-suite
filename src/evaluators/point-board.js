52b550a2b7bc7b29a7aca073f6ed3cfeb03c8378
wbigger <wbigger@gmail.com>
2018-11-29 17:35:09 +0100

    Save correction on files.

--- src/evaluators/point-board.js
+++ src/evaluators/point-board.js
@@ -14,20 +14,63 @@ var pointBoard = {
     eventHandler: function () {
         $("#lock-input").change(this.readSingleFile.bind(this));
         $("#create-board").click(this.create.bind(this));
-        $("#save-button").click(this.saveCorrections);
+        $("#save-button").click(this.saveCorrections.bind(this));
     },
     saveCorrections: function () {
-        console.log(this);
+        //console.log(this);
         let form_data = $("#board-form").serializeArray();
         for (var input of form_data) {
-            console.log(input);
-            let element = input['name'];
-            // TODO: put logic here
-        }
-        return false;
+            let inputName = input['name'];
+            let splitName =  inputName.split('-'); 
+            let itemType = splitName[0];
+            let studentId = splitName[1];
+            let classwork = this.lockObj.answers
+                .find(e => e.student.id == studentId);
+            // TODO: add an unique identifier to items and find it with it
+            //console.log(studentId);
+            if (itemType === "mq") {
+                let answerText = input['value'].replace(/ /g, '');
+                [...answerText].forEach((c, idx) => {
+                    classwork.itemList[idx]
+                        .evaluation.studentAnswer = parseInt(c);
+                });
+            } else if (itemType === "oa") {
+                let short = splitName[2].trim();
+                let item = classwork.itemList.find(e=>e.type=="open-answer");
+                let eval = item.evaluation.pointList.find(e=>
+                    e.short === short
+                );
+                eval.studentAnswer = true;
+            }
+
+        };
+        console.log(this.lockObj);
+        this.sayHello();
+        //this.saveTofile(); // why doesnt work with this function name????
+        return false; // do not continue the submit chain
     },
-    sayHello: function () {
+    sayHello: function () { //TODO: rename this function
         console.log("hello");
+        let data = JSON.stringify(this.lockObj);
+        let filename = this.lockFilename;//`lock-${app.className}-${app.subject}.json`;
+        let type = "application/json";
+        console.log(`Saving file: ${filename}`); // use string template :)
+        // from stackoverflow
+        var file = new Blob([data], { type: type });
+        if (window.navigator.msSaveOrOpenBlob) // IE10+
+            window.navigator.msSaveOrOpenBlob(file, filename);
+        else { // Others
+            var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+            a.href = url;
+            a.download = filename;
+            document.body.appendChild(a);
+            a.click();
+            setTimeout(function () {
+                document.body.removeChild(a);
+                window.URL.revokeObjectURL(url);
+            }, 0);
+        }
         return false;
     },
     create: function () {
@@ -36,24 +79,24 @@ var pointBoard = {
             let fieldset = $('<fieldset>');
             let legend = $('<legend>').text(element.student.name);
             fieldset.append(legend);
-            let textbox = $('<input>',{
-                type:'text',
-                name:`mq-${element.student.id}`,
-                id:`mq-${element.student.id}`//,
+            let textbox = $('<input>', {
+                type: 'text',
+                name: `mq-${element.student.id}`,
+                id: `mq-${element.student.id}`//,
                 //value: "hello"
             });
             fieldset.append(textbox);
-            
-            let openAnswerArray = element.itemList.filter(item => {return item.type == "open-answer"; })
+
+            let openAnswerArray = element.itemList.filter(item => { return item.type == "open-answer"; })
             openAnswer = openAnswerArray[0];
             openAnswer.evaluation.pointList.forEach(point => {
                 txt += `<span><input type="checkbox" name="" id="oa-${point.short}-${element.student.id}" value="${point.short}">${point.short}</span>`;
-                let checkbox = $('<input>',{
-                    type:'checkbox',
-                    name:`oa-${point.short}-${element.student.id}`,
-                    id:`oa-${point.short}-${element.student.id}`,
+                let checkbox = $('<input>', {
+                    type: 'checkbox',
+                    name: `oa-${element.student.id}-${point.short}`,
+                    id: `oa-${element.student.id}-${point.short}`,
                     value: `${point.short}`//,
-//                    checked: true
+                    //                    checked: true
                 });
                 let checkspan = $('<span>').append(checkbox).append(point.short);
                 fieldset.append(checkspan);
